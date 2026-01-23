123f8bb59e45ed3453ce23aa555d421d967bb3b7
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2026-01-23 14:19:53 +0100

    feat: Added cat_file() to read and decompress

--- .mygit/mygit.py
+++ .mygit/mygit.py
@@ -13,6 +13,7 @@ def init():
   
   print("Initialized empty MyGit repository in .mygit/")
   
+# This function will take file data and return its unique "address."  
 def hash_object(data, type="blob", write=True):
   # 1. Build the header
   header = f"{type} {len(data)}".encode()
@@ -23,6 +24,10 @@ def hash_object(data, type="blob", write=True):
   
   if write:
     # 3. Determine the path (e.g., objects/a1/b2c3d4...)
+    # Why the sha1[:2]? > If you put 10,000 files in one folder, 
+    # your operating system gets slow. Git splits them 
+    # into 256 folders (00 through ff) based on the first two characters of 
+    # the hash to keep things fast.
     path = os.path.join(".mygit/objects", sha1[:2], sha1[2:])
     os.makedirs(os.path.dirname(path), exist_ok=True)
     
@@ -31,3 +36,16 @@ def hash_object(data, type="blob", write=True):
       f.write(zlib.compress(full_data))
     
     return sha1
+  
+# If we can write it, we must be able to read it. To "restore" a file, 
+# we need to reverse the process: find the file, decompress it, 
+# and strip the header.
+def cat_file(sha1):
+  path = os.path.join(".mygit/objects", sha1[:2], sha1[2:])
+  with open(path, "rb") as f:
+    # Decompress
+    raw = zlib.decompress(f.read())
+    
+    # Split header and content at the null byte
+    header, content = raw.split(b"\0", 1)
+    return content.decode()
